3fd0aafde46f3bd65b28abb1e5a4acf4967fefaf
Kimoy <elishakimcatayas@gmail.com>
2022-10-10 15:16:38 +0800

    fix section 4

--- src/Components/SectionFour.jsx
+++ src/Components/SectionFour.jsx
@@ -1,26 +1,18 @@
 import neonNightClubImg from "../assets/neon-nightclub.jpg";
 import CustomButton from "./CustomButton";
+import "../styles/SectionFour.css";
 
 const SectionFour = () => {
   return (
     <div
+      className="s4-container"
       style={{
         backgroundImage: `url(${neonNightClubImg})`,
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        height: "100vh",
-        textAlign: "center",
-        display: "flex",
-        alignItems: "end",
-        justifyContent: "center",
-        color: "white",
       }}
     >
-      {/* <img alt="" src={neonNightClubImg} width="600" className="my-3 mx-3 d-inline-block align-top" /> */}
       <div style={{ paddingBottom: "3rem" }}>
         <h1 style={{ fontSize: "7rem" }}>Join The Waitlist</h1>
-        <CustomButton className="d-flex justify-content-center"  />
+        <CustomButton className="d-flex justify-content-center" />
       </div>
     </div>
   );
